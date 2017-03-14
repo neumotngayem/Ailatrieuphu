add2402bf8db5c8a70ebb51ae734be5cb5ec57d7
neumotngayem <neumotngayem@gmail.com>
2017-03-15 00:33:47 +0700

    Third Comit

---
+++ app/src/main/java/com/example/administrator/trieuphu/GameActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.trieuphu;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
